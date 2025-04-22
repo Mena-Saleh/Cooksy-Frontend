fd435e9e63ac59049229c396b66c29a11a42b710
JessicaFredin <110724442+JessicaFredin@users.noreply.github.com>
2025-04-22 16:50:10 +0200

    chore: added language changing temporarily in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
 import RegisterForm from "./components/forms/RegisterForm";
+import { useTranslation } from "react-i18next";
 
 function App() {
+	const { i18n } = useTranslation();
+
+	i18n.changeLanguage("en");
+
 	return (
 		<div className="sm:mx-10 my-10">
 			<RegisterForm />
