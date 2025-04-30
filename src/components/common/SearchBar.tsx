b429c1c37cd5219f7e58b07e9b021da9a867289c
Mena Saleh <mena.a.saleh.2001@gmail.com>
2025-04-30 18:56:48 +0300

    fix:changed search bar width to be full on phones

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -3,24 +3,24 @@ import { uiIcons } from "../../constants/uiIcons";
 import { useTranslation } from "react-i18next";
 
 export default function SearchBar() {
-	const {t: tHome} = useTranslation("home")
+	const { t: tHome } = useTranslation("home")
 	return (
 		<div className="relative flex items-center justify-center w-full">
 			{/* Input Field */}
 			<input
 				type="text"
 				placeholder={tHome("searchPlaceholder")}
-				className="input border-basic-900 w-full pr-10 px-4 py-1 sm:py-2 rounded-full bg-white focus:outline-none transition-300"
+				className="input border-basic-900 w-full pr-10 px-4 py-2 rounded-full bg-white focus:outline-none transition-300"
 			/>
 
 			{/* Search Button */}
 			<button
 				type="button"
-				className="absolute right-0 sm:right-1 m-1 w-5 h-5 sm:w-7 sm:h-7 flex items-center justify-center rounded-full bg-secondary-500 text-basic-100 transition-300 cursor-pointer hover:bg-secondary-600"
+				className="absolute right-0 sm:right-1 m-1 w-7 h-7 flex items-center justify-center rounded-full bg-secondary-500 text-basic-100 transition-300 cursor-pointer hover:bg-secondary-600"
 			>
 				<Icon
 					icon={uiIcons.actions.magnify}
-					className="w-3 h-3 sm:w-5 sm:h-5"
+					className="w-5 h-5"
 				/>
 			</button>
 		</div>
