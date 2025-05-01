3eac96cca0f45a45832273003e48834252177388
JessicaFredin <110724442+JessicaFredin@users.noreply.github.com>
2025-05-01 14:06:25 +0200

    feat: added footer and featured articles

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,31 +4,18 @@ import MainIngredientPicker from "../components/home/MainIngredientPicker";
 import ContributeBanner from "../components/home/ContributeBanner";
 // import ProfileCard from "../components/users/ProfileCard";
 import TopContributors from "../components/home/TopContributors";
+// import ArticleCard from "../components/articles/ArticleCard";
+import FeaturedArticles from "../components/home/FeaturedArticles";
 
 export default function HomePage() {
 	return (
 		<div>
 			<Hero />
-
 			<FeaturedRecipes onShowMore={() => {}} />
-
 			<MainIngredientPicker onShowMore={() => {}} />
-
 			<ContributeBanner />
-
 			<TopContributors onShowMore={() => {}} />
-
-			{/* <div className="flex justify-center items-center my-50">
-				<ProfileCard
-					rank={1}
-					imageUrl="https://eco-cdn.iqpc.com/eco/images/speakers/7l8tMmTXd9HkqHe79nhKvXvJS2RwQDmWH8YcAJv8.png"
-					name="Lisa Karlsson"
-					recipeCount={35}
-					followerCount={452}
-				/>
-			</div> */}
-
-			<div className="h-30"></div>
+			<FeaturedArticles />
 		</div>
 	);
 }
